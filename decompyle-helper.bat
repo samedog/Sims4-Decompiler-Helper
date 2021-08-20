9621c79cf9b26486c49b525428cd8fa8f3a38f7d
Diego <the.samedog@gmail.com>
2021-08-20 12:39:28 -0400

    Update decompyle-helper.bat

--- decompyle-helper.bat
+++ decompyle-helper.bat
@@ -1,239 +1,173 @@
-@rem Batch file created by Darkkitten on 9/2/2014 at 9pm PST
-@rem Batch file was updated by Darkkitten on 2/22/2019 at 5:20 CST
-@rem
-@rem Version 8.1
-@rem 
-@rem You will need Python 3.7.11  make sure you click the check box next to the add python to path
-@rem decompyle3 now automatily installs for you.
-@rem 
-@rem Did some cleanup added the latest patch to decompile
-@rem Fixed up some folder errors.
-@rem I still need to work on getting the decompyle3 version check working right.
-@rem Now checks if your tempdir exists and if not creates it instead of just doing a mkdir.
-@rem I will only maintain this on major change updates now.
-@rem I no longer need to update for Patch Versions with 8.1.
-@rem If you find any issues email me @ darkkitten at gmail dot com
-@rem 
-@rem Bat file to decompile TS4 Python code
-@rem Set your Sims4 Directory here
-@rem Set your tempdir here as well
-@rem Also Set your Zip Program
-
-@rem Setting up Versions
-@if not defined python (set py3=3.7.11)
-@if not defined decompyle3 (set dcp3=3.2.5)
-
-@rem Python
-@echo Checking Python Version
-@rem @if %py3% geq 3.7.11( @goto Finish ) else ( @goto NoPython )
-@if %py3% geq 3.7.11( @goto NoPython ) else ( @goto NoPython )
-
-@:Finish
-@python --version
-@echo You have the correct version of python.
-@echo Cecking decompyle3 Version
-@if %dcp3% geq 3.5.2 ( @goto Finish2 ) else ( @goto Installdecompyle3 )
-
-@rem decompyle3
-@:Finish2
-@echo You have the current version of decompyle3 installed.
-@decompyle3 --version
-@goto Finish3
-
-@rem Decompile Stuff
-@:Finish3
-
-@echo Go get yourself some coffee, and wait awhile, all 3 zip's used for code take forever to 
-@echo decompile, so its going to take a bit.  Enjoy!
-
-@echo Setting Directories...
-@set SIMS4DIR="GAME_DIR"
-@set TEMPDIR="TEMP_DIR_FOR_PROCESSING"
-@set ZIPPROGRAM="C:\Program Files\7-Zip\7z.exe"
-
-@echo Checking if your Temp dir exists at %TEMPDIR%
-@if exist %TEMPDIR% (@goto Continue2) else  (@goto Createfolder)
-
-@:Continue1
-
-@echo Copying base.zip to temp folder at "%TEMPDIR%"
-@copy %SIMS4DIR%\Data\Simulation\Gameplay\base.zip %TEMPDIR%
-
-@%ZIPPROGRAM% x %TEMPDIR%\base.zip -o%TEMPDIR%\libs
-
-@echo Deleting Old Base zip
-@del %TEMPDIR%\base.zip
-@echo Done
-
-@set TD=%TEMPDIR%\libs
-
-@echo Decompiling %TD% and all subfolder PYC files  (Warning Not all Files will Decompile properly) Press any key to start.
-@pause
 @echo off
+rem Batch file created by Darkkitten on 9/2/2014 at 9pm PST
+rem Batch file was updated by Darkkitten on 2/22/2019 at 5:20 CST
+rem Batch file modded by the_samedog on 08/22/2021 at 10:19 GMT-4
 
-@for /R %TD% %%f in (*.pyc) do ( 
-	decompyle3 -o "%%~df%%~pf%%~nf.py" "%%~df%%~pf%%~nf.pyc" | find "Successfully decompiled file"
-	if errorlevel 1 ( 
-		echo "could not decompile file with decompile3, trying uncompyle6"
-		uncompyle6 -o "%%~df%%~pf%%~nf.py" "%%~df%%~pf%%~nf.pyc" | find "Successfully decompiled file"
-			if errorlevel 1 ( 
-				echo "=============================================================================="
-				echo "could not decompile with uncompyle6 or deompyle3, trying unpyc3 as last resort"
-				echo "cross your fingers and check the file manually"
-				echo "%%~df%%~pf%%~nf.py"
-				echo "=============================================================================="
-				python.exe unpyc3.py "%%~df%%~pf%%~nf.pyc" > "%%~df%%~pf%%~nf.py"
-			)  else ( 
-				echo "%%~df%%~pf%%~nf.pyc with uncompyle6"
-			)
-		) else ( 
-		echo "%%~df%%~pf%%~nf.pyc with decompyle3"	
-	)
-)
+rem Setting up Versions
+set py_minver=3.7.0
+set dc_minver=3.7.6
+set uc_minver=3.7.4
 
-@REM Finished with base conversions
-@echo Done ... press any key to continue
-@pause
+FOR /F "tokens=* USEBACKQ" %%F IN (`python.exe --version`) DO (SET py3=%%F)
+set "py3=%py3:* =%"
 
-@echo Removing pyc files
-@del /s %TD%\*.pyc
+FOR /F "tokens=* USEBACKQ" %%F IN (`decompyle3.exe --version`) DO (SET dcp3=%%F)
+set "dcp3=%dcp3:* =%"
 
-@echo Zipping base folder
-@%ZIPPROGRAM% a base-src.zip  %TEMPDIR%\libs\key %TEMPDIR%\libs\lib
+FOR /F "tokens=* USEBACKQ" %%F IN (`uncompyle6.exe --version`) DO (SET ucp6=%%F)
+set "ucp6=%ucp6:* =%"
 
-@echo Deleting Files and Folders
-@rmdir /s /q %TEMPDIR%\libs
+rem version check now in a function (tm)
+CALL :VersionCheck python %py3% %py_minver%
+CALL :VersionCheck decompyle3 %dcp3% %dc_minver%
+CALL :VersionCheck uncompyle6 %ucp6% %uc_minver%
 
-@echo Done.. Press any Key to continue
-@pause
 
-@rem Core.zip
-@echo Copying core.zip to temp folder at "%TEMPDIR%"
-@copy %SIMS4DIR%\Data\Simulation\Gameplay\core.zip %TEMPDIR%
+rem Decompile Stuff
 
-@%ZIPPROGRAM% x %TEMPDIR%\core.zip -o%TEMPDIR%\core
-@echo Deleting core.zip
-@del %TEMPDIR%\core.zip
-@echo Done
+echo Go get yourself some coffee, and wait awhile, all 3 zip's used for code take forever to 
+echo decompile, so its going to take a bit.  Enjoy!
 
-@set TD=%TEMPDIR%\core
+echo Setting Directories...
+set SIMS4DIR=C:\Games\The Sims 4
+set TEMPDIR=C:\Users\demon\Documents\SIMS4 MODS\Forex ^& Investments\Temp
+set ZIPPROGRAM=C:\Program Files\7-Zip\7z.exe
+set UNPYC=C:\Users\demon\Documents\SIMS4 MODS\Forex ^& Investments\unpyc3.py
 
-@echo Decompiling %TD% and sub folders pyc files  (Warning Not all Files will Decompile properly) Press any key to start.
-@pause
-@echo off
+echo "Checking if your Temp dir exists at %TEMPDIR%"
 
-@for /R %TD% %%f in (*.pyc) do ( 
-	decompyle3 -o "%%~df%%~pf%%~nf.py" "%%~df%%~pf%%~nf.pyc" | find "Successfully decompiled file"
-	if errorlevel 1 ( 
-		echo "could not decompile file with decompile3, trying uncompyle6"
-		uncompyle6 -o "%%~df%%~pf%%~nf.py" "%%~df%%~pf%%~nf.pyc" | find "Successfully decompiled file"
-			if errorlevel 1 ( 
-				echo "=============================================================================="
-				echo "could not decompile with uncompyle6 or deompyle3, trying unpyc3 as last resort"
-				echo "cross your fingers and check the file manually"
-				echo "%%~df%%~pf%%~nf.py"
-				echo "=============================================================================="
-				python.exe unpyc3.py "%%~df%%~pf%%~nf.pyc" > "%%~df%%~pf%%~nf.py"
-			)  else ( 
-				echo "%%~df%%~pf%%~nf.pyc with uncompyle6"
-			)
-		) else ( 
-		echo "%%~df%%~pf%%~nf.pyc with decompyle3"	
-	)
+if exist "%TEMPDIR%" (
+	echo Folder exists
+) else (
+	echo "Creating %TEMPDIR%"
+	mkdir "%TEMPDIR%"
+	echo Done
+	pause
 )
 
 
-@rem Finished with Core conversions
-@echo Done ... press any key to continue
-@pause
+rem Main loop calls
+CALL :MainLoopFunction "base.zip"
+CALL :MainLoopFunction "core.zip"
+CALL :MainLoopFunction "simulation.zip"
 
-@echo Removing pyc files
-@del /s %TD%\*.pyc
+echo Done ... press any key to continue
+pause
 
-@echo Zipping Files
-@%ZIPPROGRAM% a core_src.zip %TEMPDIR%\key %TEMPDIR%\core
 
-@echo Cleaning up %TEMPDIR%
-@rmdir /s /q %TEMPDIR%\core
+rem *********************************************************************************************************
+:MainLoopFunction
+	set zipname=%~1
+	echo "entering main function with %zipname%"
+	echo "Copying %zipname% to temp folder at %TEMPDIR%"
+	copy "%SIMS4DIR%\Data\Simulation\Gameplay\%zipname%" "%TEMPDIR%"
 
-@echo Done ... press any key to continue
-@pause
+	if %zipname% == base.zip (
+		set folder_name = "libs"
+		set zip_name = "libs\lib"
+	)
+	if %zipname% == core.zip (
+		set folder_name = "core"
+		set zip_name = %folder_name%
+	)
+	if %zipname% == simulation.zip (
+		set folder_name = "simulation"
+		set zip_name = %folder_name%
+	)
 
-@rem Simulation.zip
-@echo Copying simulation.zip to temp folder at "%TEMPDIR%"
-@copy %SIMS4DIR%\Data\Simulation\Gameplay\simulation.zip %TEMPDIR%
-@cd %TEMPDIR%
+	"%ZIPPROGRAM%" x "%TEMPDIR%\%zipname%" -o"%TEMPDIR%\%folder_name%"
 
-@%ZIPPROGRAM% x %TEMPDIR%\simulation.zip -o%TEMPDIR%\simulation
-@echo Deleting simulation.zip
+	if exist "%TEMPDIR%\%zipname%" (
+		echo "Deleting Old %zipname%"
+		del "%TEMPDIR%\%zipname%"
+		echo Done
+	)
+	
+	set TD="%TEMPDIR%\%folder_name%"
 
-@del %TEMPDIR%\simulation.zip
-@echo Done
+	echo Decompiling %TD% and all subfolder PYC files (Warning Not all Files will Decompile properly) Press any key to start.
+	pause
 
-@set TD=%TEMPDIR%\simulation
+	CALL :DecompilerLoop %TD%
 
-@echo Decompiling Simulation and sub folders pyc files  (Warning Not all Files will Decompile properly) Press any key to start.
-@pause
-@echo off
+	echo Done ... press any key to continue
+	pause
 
-@for /R %TD% %%f in (*.pyc) do ( 
-	decompyle3 -o "%%~df%%~pf%%~nf.py" "%%~df%%~pf%%~nf.pyc" | find "Successfully decompiled file"
-	if errorlevel 1 ( 
-		echo "could not decompile file with decompile3, trying uncompyle6"
-		uncompyle6 -o "%%~df%%~pf%%~nf.py" "%%~df%%~pf%%~nf.pyc" | find "Successfully decompiled file"
+	echo Removing pyc files
+	del /s %TD%\*.pyc
+
+	echo Zipping %folder_name% folder
+	"%ZIPPROGRAM%" a base-src.zip "%TEMPDIR%\libs\key" "%TEMPDIR%\%zip_name%"
+
+	echo Deleting Files and Folders
+	rmdir /s /q "%TEMPDIR%\%folder_name%"
+
+	echo Done.. Press any Key to continue
+	pause
+
+
+rem this loops iterates over files and tries to decompile all pyc
+rem i use decompyle3 as main, uncompyle6 as backup and unpyc3 as last resort
+rem (reliability over performance)
+:DecompilerLoop
+	set pypath="%~1"
+	for /R %pypath% %%f in (*.pyc) do ( 
+		echo "****************************************************************************************************"
+		echo "Decompiling %%~df%%~pf%%~nf.pyc"
+		decompyle3 -o "%%~df%%~pf%%~nf.py" "%%~df%%~pf%%~nf.pyc" | find "Successfully decompiled file"
+		if errorlevel 1 ( 
+			echo "could not decompile file with decompyle3, trying uncompyle6"
+			uncompyle6 -o "%%~df%%~pf%%~nf.py" "%%~df%%~pf%%~nf.pyc" | find "Successfully decompiled file"
 			if errorlevel 1 ( 
 				echo "=============================================================================="
-				echo "could not decompile with uncompyle6 or deompyle3, trying unpyc3 as last resort"
+				echo "could not decompile trying unpyc3 as last resort"
 				echo "cross your fingers and check the file manually"
 				echo "%%~df%%~pf%%~nf.py"
 				echo "=============================================================================="
-				python.exe unpyc3.py "%%~df%%~pf%%~nf.pyc" > "%%~df%%~pf%%~nf.py"
-			)  else ( 
+				python.exe "%UNPYC%" "%%~df%%~pf%%~nf.pyc" > "%%~df%%~pf%%~nf.py"
+				if %%~z%%~df%%~pf%%~nf.py == 0  (
+					echo "could not decompile with uncompyle6 or deompyle3 or unpyc3"
+				) else (
+					echo # Successfully decompiled file
+					echo "%%~df%%~pf%%~nf.pyc with unpyc3"
+				)
+			) else ( 
 				echo "%%~df%%~pf%%~nf.pyc with uncompyle6"
 			)
 		) else ( 
-		echo "%%~df%%~pf%%~nf.pyc with decompyle3"	
+			echo "%%~df%%~pf%%~nf.pyc with decompyle3"	
+		)
 	)
-)
-
-@REM Finished with Simulation conversions
-@echo Done ... press any key to continue
-@pause
-
-@echo Deleting pyc Files
-@del /s %TD%\*.pyc
-
-@echo Compressing simulation
-@%ZIPPROGRAM% a simulation-src.zip %TEMPDIR%\key %TEMPDIR%\simulation
-
-@echo Cleaning up %TEMPDIR%
-@del %TEMPDIR%\key
-@rmdir /s /q %TEMPDIR%\simulation
-@goto FinalDone
 
 
-@:Createfolder
-@echo Creating %TEMPDIR%
-mkdir %TEMPDIR%
-@echo Done
-@pause
-@goto Continue1
+:VersionCheck
+	set program="%~1"
+	set cur_ver="%~2"
+	set min_ver="%~3"
+	set ucp_flag=0
+	echo -----------------------------------------------
+	echo Checking %program% Version
+	
+	if %program% == "python" ( set extra_steps="Please install Python 3.7 from the Microsoft store or thru conda" )
+	if %program% == "decompyle3" ( set extra_steps="Please install decompyle 3 from https://github.com/rocky/python-decompile3" )
+	if %program% == "uncompyle6" ( set ucp_flag=1	)
+	
 
-@:NoPython
-@echo you do not have the correct version of python. Please install 3.7.11
+	if "%cur_ver%" geq "%min_ver%" (
+		echo %program% version: %cur_ver% is correct
+	) else (
+		echo '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+		echo "you do not have the correct version: %min_ver%"
+		if %ucp_flag% geq 1 (
+			echo Installing uncompyle6  
+			pip install uncompyle6
+			echo Done...
+		) else (
+			echo %extra_steps%
+		)
+		echo '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+		exit
 
-
-@:Installdecompyle3
-@echo Installing decompyle3
-@pip install decompyle3
-@echo Done...
-@pause
-@goto Finish2
-
-@:Continue2
-@echo Folder exists
-@goto Continue1
-
-@:FinalDone
-@echo Done ... press any key to continue
-@pause
+	)
+	echo:
+	
