44cec1d1207e72262d305a3090d31a7f8a617be4
Diego <the.samedog@gmail.com>
2021-08-21 19:58:23 -0400

    multiple fixes
    now the script works properly instead failing on eery iteration, deleted a lot of unnecessary stops and confirmations and refined the code a little, happy about it since it's my first batch script

--- decompyle-helper.bat
+++ decompyle-helper.bat
@@ -4,9 +4,10 @@ rem Batch file was updated by Darkkitten on 2/22/2019 at 5:20 CST
 rem Batch file modded by the_samedog on 08/22/2021 at 10:19 GMT-4
 
 rem Setting up Versions
-set py_minver=3.7.0
-set dc_minver=3.7.6
-set uc_minver=3.7.4
+
+set pyminver=3.7.0
+set dcminver=3.7.6
+set ucminver=3.7.4
 
 FOR /F "tokens=* USEBACKQ" %%F IN (`python.exe --version`) DO (SET py3=%%F)
 set "py3=%py3:* =%"
@@ -18,10 +19,10 @@ FOR /F "tokens=* USEBACKQ" %%F IN (`uncompyle6.exe --version`) DO (SET ucp6=%%F)
 set "ucp6=%ucp6:* =%"
 
 rem version check now in a function (tm)
-CALL :VersionCheck python %py3% %py_minver%
-CALL :VersionCheck decompyle3 %dcp3% %dc_minver%
-CALL :VersionCheck uncompyle6 %ucp6% %uc_minver%
-
+CALL :VersionCheck python %py3% %pyminver%
+CALL :VersionCheck decompyle3 %dcp3% %dcminver%
+CALL :VersionCheck uncompyle6 %ucp6% %ucminver%
+CALL :VersionCheck unpyc3 1 1
 
 rem Decompile Stuff
 
@@ -29,10 +30,10 @@ echo Go get yourself some coffee, and wait awhile, all 3 zip's used for code tak
 echo decompile, so its going to take a bit.  Enjoy!
 
 echo Setting Directories...
-set SIMS4DIR="GAME_DIR"
-set TEMPDIR="TEMP_DIR_FOR_PROCESSING"
-set ZIPPROGRAM="C:\Program Files\7-Zip\7z.exe"
-set set UNPYC=PAH_TO_unpyc3.py
+set SIMS4DIR=C:\Games\The Sims 4
+set TEMPDIR=C:\Users\demon\Documents\SIMS4 MODS\Forex ^& Investments\Temp
+set ZIPPROGRAM=C:\Program Files\7-Zip\7z.exe
+set UNPYC=C:\Users\demon\Documents\SIMS4 MODS\Forex ^& Investments\unpyc3.py
 
 Call :CleanUp
 
@@ -44,34 +45,35 @@ CALL :MainLoopFunction "simulation.zip"
 echo Done ... press any key to continue
 pause
 
-
 rem *********************************************************************************************************
 :MainLoopFunction
 	set zipname=%~1
+	echo "Creating %TEMPDIR%"
+	mkdir "%TEMPDIR%"
 	echo "entering main function with %zipname%"
 	echo "Copying %zipname% to temp folder at %TEMPDIR%"
 	copy "%SIMS4DIR%\Data\Simulation\Gameplay\%zipname%" "%TEMPDIR%"
-
-	if %zipname% == "base.zip" (
-		set FOLDERNAME=libs
-		set ZIPPATH=libs\lib
-		set ZIPNAME=base-src.zip
-		set KEYLOCATION="%FOLDERNAME%\key"
+	
+	
+	if %zipname% == base.zip (
+		set FOLDERNAME="base"
+		rem set ZIPPATH="libs\lib"
+		rem set FINALZIPNAME="base-src.zip"
+		rem set KEYLOCATION="%FOLDERNAME%\key"
 	)
-	if %zipname% == "core.zip" (
+	if %zipname% == core.zip (
 		set FOLDERNAME=core
-		set ZIPPATH=core
-		set ZIPNAME=core-src.zip
-		set KEYLOCATION=key
+		rem set ZIPPATH=core
+		rem set FINALZIPNAME="core-src.zip"
+		rem set KEYLOCATION=key
 	)
-	if %zipname% == "simulation.zip" (
-		set FOLDERNAME=simulation
-		set ZIPPATH=simulation
-		set ZIPNAME=simulation-src.zip
-		set KEYLOCATION=key
+	if %zipname% == simulation.zip (
+		set FOLDERNAME="simulation"
+		rem set ZIPPATH="simulation"
+		rem set FINALZIPNAME="simulation-src.zip"
+		rem set KEYLOCATION="key"
 	)
-
-
+	
 	"%ZIPPROGRAM%" x "%TEMPDIR%\%zipname%" -o"%TEMPDIR%\%FOLDERNAME%"
 
 	if exist "%TEMPDIR%\%zipname%" (
@@ -81,24 +83,20 @@ rem ****************************************************************************
 	)
 	
 	set TD="%TEMPDIR%\%FOLDERNAME%"
-
 	echo Decompiling %TD% and all subfolder PYC files (Warning Not all Files will Decompile properly) Press any key to start.
-	pause
-
+	
 	CALL :DecompilerLoop %TD%
 
 	echo Removing pyc files
 	del /s %TD%\*.pyc
 
-	echo Zipping %folder_name% folder
-	"%ZIPPROGRAM%" a "%ZIPNAME%" "%TEMPDIR%\%KEYLOCATION%" "%TEMPDIR%\%ZIPPATH%"
-
+	rem echo Zipping %folder_name% folder
+	rem "%ZIPPROGRAM%" a %FINALZIPNAME% "%TEMPDIR%\%KEYLOCATION%" "%TEMPDIR%\%ZIPPATH%"
+	echo Moving folder to tree root
+	move "%TEMPDIR%\%FOLDERNAME%" "%FOLDERNAME%"
+	
 	echo Deleting Files and Folders
-	
 	Call :CleanUp
-	
-	echo Done.. Press any Key to continue
-	pause
 EXIT /B 0
 
 rem this loops iterates over files and tries to decompile all pyc
@@ -140,46 +138,57 @@ EXIT /B 0
 	set cur_ver="%~2"
 	set min_ver="%~3"
 	set ucp_flag=0
+	set unpyc3_flag=0
 	echo -----------------------------------------------
 	echo Checking %program% Version
 	
 	if %program% == "python" ( set extra_steps="Please install Python 3.7 from the Microsoft store or thru conda" )
 	if %program% == "decompyle3" ( set extra_steps="Please install decompyle 3 from https://github.com/rocky/python-decompile3" )
-	if %program% == "uncompyle6" ( set ucp_flag=1	)
+	if %program% == "uncompyle6" ( set ucp_flag=1 )
+	if %program% == "unpyc3" ( set unpyc3_flag=1 )
 	
-
-	if "%cur_ver%" geq "%min_ver%" (
-		echo %program% version: %cur_ver% is correct
-	) else (
-		echo '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
-		echo "you do not have the correct version: %min_ver%"
-		if %ucp_flag% geq 1 (
-			echo Installing uncompyle6  
-			pip install uncompyle6
-			echo Done...
+	if "%unpyc3_flag%" geq "%min_ver%" (
+		if exist "unpyc3.py" (
+			echo unpyc3 is installed.
 		) else (
-			echo %extra_steps%
+			bitsadmin.exe /transfer "unpyc3" "https://raw.githubusercontent.com/andrew-tavera/unpyc37/master/unpyc3.py" "%~dp0\unpyc3.py"
 		)
-		echo '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
-		exit
-
+	) else (
+		if "%cur_ver%" geq "%min_ver%" (
+			echo %program% version: %cur_ver% is correct
+		) else (
+			echo '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+			echo "you do not have the correct version: %min_ver%"
+			if %ucp_flag% geq 1 (
+				echo Installing uncompyle6  
+				pip install uncompyle6
+				echo Done...
+			) else (
+				echo %extra_steps%
+			)
+			echo '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+			exit
+		)
 	)
 	echo:
 EXIT /B 0
 
 :CleanUp
-	echo Checking if your Temp dir exists at "%TEMPDIR%"
-	if exist "%TEMPDIR%" (
-		echo File exists
-		del "%TEMPDIR%"
-	)
-	if exist "%TEMPDIR%"\ (
-		echo Folder exists
-		rmdir "%TEMPDIR%" /q /s
-	)
+	CALL :DeleteSafe temp
+	CALL :DeleteSafe base
+	CALL :DeleteSafe core
+	CALL :DeleteSafe simulation
+EXIT /B 0
 
-	echo "Creating %TEMPDIR%"
-	mkdir "%TEMPDIR%"
-	echo Done
-	pause
+:DeleteSafe
+	set folder=%~1
+	echo Checking if your simulation dir exists at %folder%
+	if exist "%folder%" (
+		echo File exists
+		del /f /q "%folder%"
+	)
+	if exist "%folder%" (
+		echo Folder exists
+		rmdir /q /s "%folder%"
+	)
 EXIT /B 0
